073b3c737f312049b3532e081eb8e3c1e0fafbbc
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-01-12 13:29:06 +0800

    Update utility.js

--- rsg-html-analyzer/scripts/utility.js
+++ rsg-html-analyzer/scripts/utility.js
@@ -11,30 +11,87 @@ const utility = {
         }
         return buffers;
     },
+    splitBuffer(buffer, code) {
+        const buffers = [];
+        let inBuffer = false;
+        let start;
+        let offset = 0;
+        const length = buffer.length;
+        while (offset < length) {
+            if (inBuffer) {
+                if (buffer[offset] === code) {
+                    buffers.push(buffer.subarray(start, offset));
+                    inBuffer = false;
+                }
+            } else {
+                if (buffer[offset] !== code) {
+                    start = offset;
+                    inBuffer = true;
+                }
+            }
+            offset += 1;
+        }
+        if (inBuffer) {
+            buffer.subarray(start);
+        }
+        return buffers;
+    },
     getHex(code) {
-        return "[" + code.toString(16).toUpperCase().padStart(2, "0") + "]";
+        return code.toString(16).toUpperCase().padStart(2, "0");
     },
     decode(codeTable, buffer) {
         let string = "";
-        let i = 0;
+        let offset = 0;
         const length = buffer.length;
-        while (i < length) {
-            const code = buffer[i];
+        while (offset < length) {
+            const code = buffer[offset];
             if (code === 0x50) {
                 break;
             }
             if (code > 0x50) {
                 string += codeTable[code - 0x50];
             } else {
-                if (code >= 0x20 && code <= 0x23 && i + 1 < length) {
-                    string += codeTable[(code - 0x20) * 0x100 + buffer[i + 1]];
-                    i += 1;
+                if (code >= 0x20 && code <= 0x23 && offset + 1 < length) {
+                    string += codeTable[
+                        (code - 0x20) * 0x100 + buffer[offset + 1]
+                    ];
+                    offset += 1;
                 } else {
-                    string += utility.getHex(code);
+                    string += "[" + utility.getHex(code) + "]";
+                }
+            }
+            offset += 1;
+        }
+        return string;
+    },
+    encode(codeTable, string, length) {
+        const stringLength = string.length;
+        const buffer = new Uint8Array(length ?? stringLength * 2);
+        let i = 0;
+        let offset = 0;
+        buffer.fill(0x50);
+        while (i < stringLength && offset < buffer.length) {
+            let code = codeTable.indexOf(string.charAt(i));
+            if (code > -1) {
+                if (code < 0xb0) {
+                    if (offset + 1 <= buffer.length) {
+                        buffer.set([code + 0x50], offset);
+                    }
+                    offset += 1;
+                } else {
+                    if (offset + 2 <= buffer.length) {
+                        const code1 = code % 0x100;
+                        code = (code - code1) / 0x100 + 0x20;
+                        buffer.set([code, code1], offset);
+                    }
+                    offset += 2;
                 }
             }
             i += 1;
         }
-        return string;
+        if (length === undefined) {
+            return buffer.subarray(0, buffer.indexOf(0x50));
+        }
+        return buffer;
     }
 };
