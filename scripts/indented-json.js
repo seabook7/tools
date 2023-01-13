b84f8c9f8530fb339bcb5ccc07b2d1e01b106f28
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-01-13 11:04:08 +0800

    Update indented-json.js

--- scripts/indented-json.js
+++ scripts/indented-json.js
@@ -1,14 +1,39 @@
 /*jslint browser*/
 /*global fileIO*/
+const fileName = document.getElementById("file-name");
 const openButton = document.getElementById("open-button");
+const saveButton = document.getElementById("save-button");
 const spaceInput = document.getElementById("space-input");
+const jsonText = document.getElementById("json-text");
+let value;
+function buildText(value, space) {
+    const int = (
+        space === ""
+        ? 4
+        : parseInt(space)
+    );
+    return JSON.stringify(value, null, (
+        Number.isNaN(int)
+        ? space
+        : int
+    ));
+}
 openButton.addEventListener("click", async function () {
     const file = await fileIO.open("application/json");
-    const value = JSON.parse(await file.text());
-    const space = parseInt(spaceInput.value);
-    fileIO.download(new Blob([JSON.stringify(value, null, (
-        Number.isNaN(space)
-        ? spaceInput.value
-        : space
-    ))]), file.name);
+    fileName.value = file.name;
+    value = JSON.parse(await file.text());
+    jsonText.replaceChildren(buildText(value, spaceInput.value));
+});
+saveButton.addEventListener("click", function () {
+    if (jsonText.firstChild) {
+        fileIO.download(
+            new Blob([jsonText.firstChild.nodeValue]),
+            fileName.value
+        );
+    }
+});
+spaceInput.addEventListener("change", function () {
+    jsonText.replaceChildren(
+        buildText(value, spaceInput.value)
+    );
 });
