ae50437aeea8828cf8c4f6d0cd709df2133e32e1
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-01-16 15:00:07 +0800

    增加 getBitFlags 方法

--- rsg-data-viewer/scripts/utility.js
+++ rsg-data-viewer/scripts/utility.js
@@ -1,4 +1,20 @@
 const utility = {
+    getBitFlags(bytes) {
+        const flags = new Array(bytes.length * 8);
+        bytes.forEach(function (byte, index) {
+            let exponent = 8;
+            while (exponent > 0) {
+                exponent -= 1;
+                const value = 2 ** exponent;
+                const bitIndex = index * 8 + exponent;
+                flags[bitIndex] = byte >= value;
+                if (flags[bitIndex]) {
+                    byte -= value;
+                }
+            }
+        });
+        return flags;
+    },
     getFixedLengthBuffers(buffer, offset, length, number) {
         const buffers = new Array(number);
         let i = 0;
