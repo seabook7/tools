d58216a19727c71a24348e387bfa1e145f4f9e7c
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-01-16 15:04:03 +0800

    Update rsg3-character.js

--- rsg-data-viewer/scripts/rsg3-character.js
+++ rsg-data-viewer/scripts/rsg3-character.js
@@ -1,16 +1,26 @@
-/*jslint bitwise, browser, unordered*/
+/*jslint bitwise, unordered*/
 /*global utility, rsg3Encoding*/
-const rsg3character = (function () {
-    function readData(buffer) {
+const rsg3Character = function (rsg3Rom) {
+    function readData0(buffer) {
         const earthMagicTable = ["souryu", "syuchoh", "byakko", "genbu"];
         const heavenMagicTable = ["sun", "moon"];
         return {
             looks: buffer[0],
             id: buffer[1],
-            flagHP: buffer[4] >>> 7,
-            hp: (buffer[4] & 0x7f) << 8 | buffer[3],
+            manFlag: buffer[2] >>> 7,
+            womanFlag: (buffer[2] & 0x40) >>> 6,
+            undeadFlag: (buffer[2] & 0x20) >>> 5,
+            unknownFlags: [
+                (buffer[2] & 0x10) >>> 4,
+                (buffer[2] & 0x08) >>> 3,
+                (buffer[2] & 0x04) >>> 2,
+                (buffer[2] & 0x02) >>> 1,
+                buffer[2] & 0x01
+            ],
+            hpFlag: buffer[4] >>> 7,
+            hp: (buffer[4] & 0x7F) << 8 | buffer[3],
             flagLP: buffer[5] >>> 7,
-            lp: buffer[5] & 0x7f,
+            lp: buffer[5] & 0x7F,
             strength: buffer[6],
             dexterity: buffer[7],
             agility: buffer[8],
@@ -18,77 +28,72 @@ const rsg3character = (function () {
             force: buffer[10],
             will: buffer[11],
             fascination: buffer[12],
-            slash: buffer[13] & 0x7f,
-            beat: buffer[14] & 0x7f,
-            thrust: buffer[15] & 0x7f,
-            shoot: buffer[16] & 0x7f,
-            wrestle: buffer[17] & 0x7f,
+            slashFlag: buffer[13] >>> 7,
+            slash: buffer[13] & 0x7F,
+            beatFlag: buffer[14] >>> 7,
+            beat: buffer[14] & 0x7F,
+            thrustFlag: buffer[15] >>> 7,
+            thrust: buffer[15] & 0x7F,
+            shootFlag: buffer[16] >>> 7,
+            shoot: buffer[16] & 0x7F,
+            wrestleFlag: buffer[17] >>> 7,
+            wrestle: buffer[17] & 0x7F,
             earthMagicType: earthMagicTable[buffer[18] >>> 5 & 0x03],
-            earthMagicLevel: buffer[18] & 0x1f,
+            earthMagicLevel: buffer[18] & 0x1F,
             heavenMagicType: heavenMagicTable[buffer[19] >>> 5 & 0x03],
-            heavenMagicLevel: buffer[19] & 0x1f,
-            levelUnknown: buffer[20] & 0x7f,
-            typeStar: buffer[40],
-            typeWeapon: buffer[42],
-            typeMag: buffer[41],
-            typeTechLearn: buffer[22],
-            typeLevelUp: buffer[21],
-            formationNo: buffer[39],
-            /* フラグ系データ */
-            flagMan: (buffer[2] & 0x80) !== 0x00,
-            flagWoman: (buffer[2] & 0x40) !== 0x00,
-            flagUndead: (buffer[2] & 0x20) !== 0x00,
-            flagUnknown: (buffer[2] & 0x1f).toString(2).padStart(5, "0"),
-            flagGrowHitPoint: (buffer[4] & 0x80) !== 0x00,
-            flagGrowLevelSword: (buffer[13] & 0x80) !== 0x00,
-            flagGrowLevelAxe: (buffer[14] & 0x80) !== 0x00,
-            flagGrowLevelSpear: (buffer[15] & 0x80) !== 0x00,
-            flagGrowLevelBow: (buffer[16] & 0x80) !== 0x00,
-            flagGrowLevelPunch: (buffer[17] & 0x80) !== 0x00,
-            flagGrowLevelEarth: (buffer[18] & 0x80) !== 0x00,
-            flagGrowLevelHeaven: (buffer[19] & 0x80) !== 0x00,
-            flagGrowLevelUnknown: (buffer[20] & 0x80) !== 0x00,
-            /* 修得している技 */
-            waza: [buffer[23], buffer[24], buffer[25], buffer[26]],
-            /* 修得している術 */
-            jyutu: [buffer[27], buffer[28], buffer[29], buffer[30]],
-            /* 装備アイテム */
+            heavenMagicLevel: buffer[19] & 0x1F,
+            generalLevelFlag: buffer[20] >>> 7,
+            generalLevel: buffer[20] & 0x7F,
+            levelUpType: buffer[21],
+            skillGetting: buffer[22],
+            skill: [buffer[23], buffer[24], buffer[25], buffer[26]],
+            magic: [buffer[27], buffer[28], buffer[29], buffer[30]],
             weapon: [buffer[31], buffer[32], buffer[33], buffer[34]],
-            armor: [buffer[35], buffer[36], buffer[37], buffer[38]]
+            armor: [buffer[35], buffer[36], buffer[37], buffer[38]],
+            formationOwn: buffer[39],
+            star: buffer[40],
+            defaultMagicType: buffer[41],
+            favorite: buffer[42],
+            unknown: [
+                buffer[43],
+                buffer[44],
+                buffer[45],
+                buffer[46],
+                buffer[47]
+            ]
         };
     }
-    function readLevelUp(buffer) {
+    function readData1(buffer) {
         return {
-            lvup_hit_point  : buffer[15],
-            lvup_tech_point : buffer[13],
-            lvup_mag_point  : buffer[14],
-            lvup_sword      : buffer[ 0],
-            lvup_axe        : buffer[ 1],
-            lvup_spear      : buffer[ 2],
-            lvup_bow        : buffer[ 3],
-            lvup_punch      : buffer[ 4],
-            lvup_wind       : buffer[ 5],
-            lvup_fire       : buffer[ 6],
-            lvup_earth      : buffer[ 7],
-            lvup_water      : buffer[ 8],
-            lvup_sun        : buffer[ 9],
-            lvup_moon       : buffer[10]
+            slash: buffer[0],
+            beat: buffer[1],
+            thrust: buffer[2],
+            shoot: buffer[3],
+            wrestle: buffer[4],
+            souryu: buffer[5],
+            syuchoh: buffer[6],
+            byakko: buffer[7],
+            genbu: buffer[8],
+            sun: buffer[9],
+            moon: buffer[10],
+            generalLevel: buffer[11],
+            unknown: buffer[12],
+            sp: buffer[13],
+            mp: buffer[14],
+            hp: buffer[15]
         };
     }
-    return function (rsg3rom) {
-        const names = utility.getBuffers(rsg3rom, 0x3d1480, 8, 48).map(
+    return {
+        names: utility.getFixedLengthBuffers(rsg3Rom, 0x3D1480, 8, 256).map(
             (buffer) => rsg3Encoding.decode(buffer)
-        );
-        const data = utility.getBuffers(rsg3rom, 0x3e0600, 48, 48).map(
-            (buffer) => readData(buffer)
-        );
-        const levelUp = utility.getBuffers(rsg3rom, 0x3e31bf, 16, 48).map(
-            (buffer) => readLevelUp(buffer)
-        );
-        return {
-            names,
-            data,
-            levelUp
-        };
+        ),
+        data: [
+            utility.getFixedLengthBuffers(rsg3Rom, 0x3E0600, 48, 48).map(
+                (buffer) => readData0(buffer)
+            ),
+            utility.getFixedLengthBuffers(rsg3Rom, 0x3E31BF, 16, 48).map(
+                (buffer) => readData1(buffer)
+            )
+        ]
     };
-}());
+};
