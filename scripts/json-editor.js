f6372b7bec0546f1ecaa3af25f5f53be2adaf231
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-02-09 15:24:49 +0800

    Update json-editor.js

--- scripts/json-editor.js
+++ scripts/json-editor.js
@@ -6,19 +6,41 @@ const saveButton = document.getElementById("save-button");
 const spaceCheckbox = document.getElementById("space-checkbox");
 const spaceInput = document.getElementById("space-input");
 const tree = document.getElementById("tree");
+function getBlob(value) {
+    let space = spaceInput.value;
+    const int = (
+        space === ""
+        ? 4
+        : parseInt(space)
+    );
+    return (
+        spaceCheckbox.checked
+        ? new Blob([JSON.stringify(value, null, (
+            Number.isNaN(int)
+            ? space
+            : int
+        ))])
+        : new Blob([JSON.stringify(value)])
+    );
+}
 spaceInput.disabled = !spaceCheckbox.checked;
 openButton.addEventListener("click", async function () {
     const file = await fileIO.open("application/json");
     fileName.value = file.name;
     try {
-        tree.replaceChildren(editableTree.from(await file.text()));
+        const value = JSON.parse(await file.text());
+        tree.replaceChildren(editableTree.from(value));
     } catch (error) {
         window.alert(error.message);
     }
 });
 saveButton.addEventListener("click", function () {
-    editableTree.to(tree.firstChild);
+    const {value} = editableTree.toValue(tree.firstChild);
+    fileIO.download(getBlob(value), fileName.value);
 });
 spaceCheckbox.addEventListener("click", function () {
     spaceInput.disabled = !spaceCheckbox.checked;
+    if (!spaceCheckbox.checked) {
+        spaceInput.value = "";
+    }
 });
