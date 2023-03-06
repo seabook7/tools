54059e0f062221e978a387cf34b69d2a8361a27e
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-03-06 13:47:10 +0800

    Update editable-table.js

--- scripts/editable-table.js
+++ scripts/editable-table.js
@@ -1,26 +1,11 @@
 /*jslint browser*/
 const editableTable = (function () {
-    function e(tagName, attributes) {
-        const element = document.createElement(tagName);
-        if (typeof attributes === "object") {
-            Object.entries(attributes).forEach(function ([key, value]) {
-                if (typeof value === "function") {
-                    element.addEventListener(key, value);
-                } else {
-                    element[key] = value;
-                }
-            });
-        }
-        return element;
-    }
     const tableClass = (
         "table table-success table-striped table-bordered w-auto"
     );
     const headClass = "table-light";
     const headCellClass = "position-static min-inline-size-3rem p-0";
-    const insertCellClass = (
-        "min-inline-size-3rem py-1 text-center cursor-pointer"
-    );
+    const insertCellClass = "min-inline-size-3rem py-1 text-center";
     const fieldCellClass = "min-inline-size-3rem p-1 white-space-pre";
     const dropdownButtonClass = (
         "dropdown-toggle border-0 w-100 p-1 bg-transparent"
@@ -28,11 +13,11 @@ const editableTable = (function () {
     const dropdownMenuClass = "dropdown-menu glass-background shadow";
     const dropdownItemClass = "dropdown-item";
     const dropendClass = "dropend";
-    const table = e("table", {className: tableClass});
-    const head = e("thead", {className: headClass});
-    const headRow = e("tr");
-    const headCell = e("th", {scope: "col"});
-    const body = e("tbody");
+    const table = document.createElement("table");
+    const head = document.createElement("thead");
+    const headRow = document.createElement("tr");
+    const headCell = document.createElement("th");
+    const body = document.createElement("tbody");
     function getColumnName(number) {
         if (number < 1) {
             return "";
@@ -55,29 +40,26 @@ const editableTable = (function () {
         }
     }
     function createColumnHeadCell(dropdownButton, dropdownMenu) {
-        const cell = e("th", {
-            className: headCellClass,
-            scope: "col"
-        });
+        const cell = document.createElement("th");
+        cell.className = headCellClass;
+        cell.scope = "col";
         cell.append(dropdownButton, dropdownMenu);
         return cell;
     }
     function createRowHeadCell(dropdownButton, dropdownMenu) {
-        const cell = e("th", {
-            className: [headClass, headCellClass, dropendClass].join(" "),
-            scope: "row"
-        });
+        const cell = document.createElement("th");
+        cell.className = [headClass, headCellClass, dropendClass].join(" ");
+        cell.scope = "row";
         cell.append(dropdownButton, dropdownMenu);
         return cell;
     }
     function createFieldCell(field) {
-        const cell = e("td", {
-            className: fieldCellClass,
-            contentEditable: "true"
-        });
+        const cell = document.createElement("td");
+        cell.className = fieldCellClass;
+        cell.contentEditable = "true";
         cell.append(
             (field === undefined || field === "")
-            ? e("br")
+            ? document.createElement("br")
             : field
         );
         return cell;
@@ -101,78 +83,74 @@ const editableTable = (function () {
         return button;
     }
     function createColumnDropdownMenu() {
-        const ul = e("ul", {className: dropdownMenuClass});
-        let li = e("li");
-        let button = e("button", {
-            className: dropdownItemClass,
-            click() {
-                insertColumn(ul.parentElement.cellIndex);
-            },
-            type: "button"
+        const ul = document.createElement("ul");
+        const insertLi = document.createElement("li");
+        const insertButton = document.createElement("button");
+        const deleteLi = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        ul.className = dropdownMenuClass;
+        insertButton.className = dropdownItemClass;
+        insertButton.type = "button";
+        insertButton.addEventListener("click", function () {
+            insertColumn(ul.parentElement.cellIndex);
         });
-        button.append(createInsertIcon(), " Insert column");
-        li.append(button);
-        ul.append(li);
-        li = e("li");
-        button = e("button", {
-            className: dropdownItemClass,
-            click() {
-                deleteColumn(ul.parentElement.cellIndex);
-            },
-            type: "button"
+        deleteButton.className = dropdownItemClass;
+        deleteButton.type = "button";
+        deleteButton.addEventListener("click", function () {
+            deleteColumn(ul.parentElement.cellIndex);
         });
-        button.append(createDeleteIcon(), " Delete column");
-        li.append(button);
-        ul.append(li);
+        insertButton.append(createInsertIcon(), " Insert column");
+        insertLi.append(insertButton);
+        deleteButton.append(createDeleteIcon(), " Delete column");
+        deleteLi.append(deleteButton);
+        ul.append(insertLi, deleteLi);
         return ul;
     }
     function createRowDropendMenu() {
-        const ul = e("ul", {className: dropdownMenuClass});
-        let li = e("li");
-        let button = e("button", {
-            className: dropdownItemClass,
-            click() {
-                insertRow(ul.parentElement.parentElement.rowIndex);
-            },
-            type: "button"
+        const ul = document.createElement("ul");
+        const insertLi = document.createElement("li");
+        const insertButton = document.createElement("button");
+        const deleteLi = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        ul.className = dropdownMenuClass;
+        insertButton.className = dropdownItemClass;
+        insertButton.type = "button";
+        insertButton.addEventListener("click", function () {
+            insertRow(ul.parentElement.parentElement.rowIndex);
         });
-        button.append(createInsertIcon(), " Insert row");
-        li.append(button);
-        ul.append(li);
-        li = e("li");
-        button = e("button", {
-            className: dropdownItemClass,
-            click() {
-                deleteRow(ul.parentElement.parentElement.rowIndex);
-            },
-            type: "button"
+        deleteButton.className = dropdownItemClass;
+        deleteButton.type = "button";
+        deleteButton.addEventListener("click", function () {
+            deleteRow(ul.parentElement.parentElement.rowIndex);
         });
-        button.append(createDeleteIcon(), " Delete row");
-        li.append(button);
-        ul.append(li);
+        insertButton.append(createInsertIcon(), " Insert row");
+        insertLi.append(insertButton);
+        deleteButton.append(createDeleteIcon(), " Delete row");
+        deleteLi.append(deleteButton);
+        ul.append(insertLi, deleteLi);
         return ul;
     }
     function createInsertColumnCell() {
-        const cell = e("th", {
-            className: insertCellClass,
-            click() {
-                insertColumn(cell.cellIndex);
-            },
-            scope: "col",
-            title: "Insert column"
+        const cell = document.createElement("th");
+        cell.className = insertCellClass;
+        cell.title = "Insert column";
+        cell.setAttribute("role", "button");
+        cell.scope = "col";
+        cell.addEventListener("click", function () {
+            insertColumn(cell.cellIndex);
         });
         cell.append(createInsertIcon());
         return cell;
     }
     function createInsertRow() {
-        const row = e("tr");
-        const cell = e("th", {
-            className: [headClass, insertCellClass].join(" "),
-            click() {
-                insertRow(row.rowIndex);
-            },
-            scope: "row",
-            title: "Insert row"
+        const row = document.createElement("tr");
+        const cell = document.createElement("th");
+        cell.className = [headClass, insertCellClass].join(" ");
+        cell.title = "Insert row";
+        cell.setAttribute("role", "button");
+        cell.scope = "row";
+        cell.addEventListener("click", function () {
+            insertRow(row.rowIndex);
         });
         cell.append(createInsertIcon());
         row.append(cell);
@@ -202,7 +180,7 @@ const editableTable = (function () {
         }
     }
     function insertRow(index) {
-        const row = e("tr");
+        const row = document.createElement("tr");
         let i = headRow.cells.length - 2;
         const rows = table.rows;
         row.append(createRowHeadCell(
@@ -250,7 +228,7 @@ const editableTable = (function () {
         }
         headRow.append(createInsertColumnCell());
         while (j < rowsCount) {
-            const row = e("tr");
+            const row = document.createElement("tr");
             j += 1;
             row.append(createRowHeadCell(
                 createDropdownButton(j),
@@ -265,6 +243,9 @@ const editableTable = (function () {
         }
         body.append(createInsertRow());
     }
+    table.className = tableClass;
+    head.className = headClass;
+    headCell.scope = "col";
     head.append(headRow);
     table.append(head, body);
     return {
