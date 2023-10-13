94d588a57630af6b71e18466e9c9325e90e101b4
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-10-13 14:42:25 +0800

    Update jisx0201-encoding.js

--- rsg-data-viewer/scripts/jisx0201-encoding.js
+++ rsg-data-viewer/scripts/jisx0201-encoding.js
@@ -1,17 +1,17 @@
 const jisx0201Encoding = {
     decode(buffer) {
-        let string = "";
+        let text = "";
         buffer.forEach(function (code) {
             if (code === 0x5C) {
-                string += "¥";
+                text += "¥";
             } else if (code === 0x7E) {
-                string += "‾";
+                text += "‾";
             } else if (code >= 0xA1 && code <= 0xDF) {
-                string += String.fromCharCode(code + 0xFEC0);
+                text += String.fromCharCode(code + 0xFEC0);
             } else {
-                string += String.fromCharCode(code);
+                text += String.fromCharCode(code);
             }
         });
-        return string;
+        return text;
     }
 };
