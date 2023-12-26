e051da2dbd4138ef601c4e112a72a3d5b0c4b08d
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-12-26 11:17:53 +0800

    Update file-io.js

--- scripts/file-io.js
+++ scripts/file-io.js
@@ -1,64 +1,85 @@
 /*jslint browser*/
-const fileIO = {
+const FileIO = (function (create) {
     /**
-     * @param {Blob} blob
-     * @param {string} [name]
+     * @param {BlobPart|BlobPart[]} blobPart
+     * @param {string} name
      */
-    download(blob, name) {
-        const anchor = document.createElement("a");
-        anchor.href = URL.createObjectURL(blob);
-        anchor.download = (
-            name === undefined
-            ? (
-                blob.name === undefined
-                ? ""
-                : blob.name
-            )
-            : name
+    function download(blobPart, name = "") {
+        const blob = new Blob(
+            Array.isArray(blobPart)
+            ? blobPart
+            : [blobPart]
         );
+        const anchor = create("a");
+        anchor.href = URL.createObjectURL(blob);
+        anchor.download = name;
         anchor.click();
         URL.revokeObjectURL(anchor.href);
-    },
+    }
+    function open(accept, multiple) {
+        const fileInput = create("input");
+        fileInput.type = "file";
+        if (accept !== undefined) {
+            fileInput.accept = accept;
+        }
+        if (multiple !== undefined) {
+            fileInput.multiple = multiple;
+        }
+        function executor(resolve) {
+            const resolveFiles = () => resolve(fileInput.files);
+            const resolveFile = () => resolve(fileInput.files[0]);
+            const cancel = () => resolve();
+            fileInput.addEventListener("change", (
+                multiple
+                ? resolveFiles
+                : resolveFile
+            ));
+            fileInput.addEventListener("cancel", cancel);
+        }
+        const promise = new Promise(executor);
+        fileInput.click();
+        return promise;
+    }
     /**
      * @param {string} [accept]
-     * @param {boolean} [multiple]
-     * @returns {Promise<File|FileList>}
+     * @returns {Promise<File>}
      */
-    open(accept, multiple) {
-        return new Promise(function (resolve) {
-            const fileInput = document.createElement("input");
-            fileInput.type = "file";
-            if (accept !== undefined) {
-                fileInput.accept = accept;
-            }
-            if (multiple !== undefined) {
-                fileInput.multiple = multiple;
-            }
-            fileInput.addEventListener("change", function () {
-                resolve(
-                    fileInput.multiple
-                    ? fileInput.files
-                    : fileInput.files[0]
-                );
-            });
-            fileInput.addEventListener("cancel", function () {
-                resolve();
-            });
-            fileInput.click();
-        });
-    },
+    const openFile = (accept) => open(accept);
     /**
-     * @param {Blob} blob
-     * @param {string} [encoding]
-     * @returns {string}
+     * @param {string} [accept]
+     * @returns {Promise<FileList>}
      */
-    readAsText(blob, encoding) {
-        return new Promise(function (resolve) {
-            const reader = new FileReader();
-            reader.addEventListener("loadend", function () {
-                resolve(reader.result);
-            });
-            reader.readAsText(blob, encoding);
-        });
+    const openFiles = (accept) => open(accept, true);
+    /**
+     * @param {File} file
+     * @returns {Promise<Uint8Array>}
+     */
+    async function readBytes(file) {
+        const arrayBuffer = await file.arrayBuffer();
+        const uint8Array = new Uint8Array(arrayBuffer);
+        return uint8Array;
     }
-};
+    function readAsText(file, encoding) {
+        const reader = new FileReader();
+        function executor(resolve, reject) {
+            const resolveResult = () => resolve(reader.result);
+            const rejectError = () => reject(reader.error);
+            reader.addEventListener("load", resolveResult);
+            reader.addEventListener("error", rejectError);
+        }
+        const promise = new Promise(executor);
+        reader.readAsText(file, encoding);
+        return promise;
+    }
+    /**
+     * @param {File} file
+     * @param {string} [encoding]
+     * @returns {Promise<string>}
+     */
+    const readText = (file, encoding) => (
+        encoding === undefined
+        ? file.text()
+        : readAsText(file, encoding)
+    );
+    return {download, openFile, openFiles, readBytes, readText};
+}((tagName) => document.createElement(tagName)));
