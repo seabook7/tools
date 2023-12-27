1c3f87846d787b08bf162c13f1ef04f56b9ff5ea
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-12-27 17:26:02 +0800

    Create crc.js

---
+++ scripts/crc.js
@@ -0,0 +1,42 @@
+/*jslint bitwise, browser, this*/
+const CRC = (function () {
+    const table = new Uint32Array(256);
+    (function () {
+        const kPoly = 0xEDB88320;
+        let i = 0;
+        while (i < 256) {
+            let r = i;
+            let j = 0;
+            while (j < 8) {
+                if ((r & 1) !== 0) {
+                    r = (r >>> 1) ^ kPoly;
+                } else {
+                    r >>>= 1;
+                }
+                j += 1;
+            }
+            table[i] = r;
+            i += 1;
+        }
+    }());
+    const toUint32 = (value) => (
+        value < 0
+        ? value + 0x100000000
+        : value
+    );
+    const getDigest = (value) => value ^ 0xFFFFFFFF;
+    const updateByte = (value, b) => table[value & 0xFF ^ b] ^ (value >>> 8);
+    const update = (data) => data.reduce(updateByte, 0xFFFFFFFF);
+    /**
+     * @param {Uint8Array|Uint8ClampedArray|number[]} data
+     * @returns {number}
+     */
+    const calculateDigest = (data) => toUint32(getDigest(update(data)));
+    /**
+     * @param {number} digest
+     * @param {Uint8Array|Uint8ClampedArray|number[]} data
+     * @returns {Promise<boolean>}
+     */
+    const verifyDigest = (digest, data) => calculateDigest(data) === digest;
+    return {calculateDigest, verifyDigest};
+}());
