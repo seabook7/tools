84a32aa54156ff5299e6958814f1958e5aeb9b50
seabook7 <40333849+seabook7@users.noreply.github.com>
2023-12-28 10:37:31 +0800

    Update crc.js

--- scripts/crc.js
+++ scripts/crc.js
@@ -1,37 +1,29 @@
 /*jslint bitwise*/
 const CRC = (function () {
-    const table = new Uint32Array(256);
-    (function () {
+    const table = (function () {
+        const array256 = new Uint32Array(256).fill(0);
+        const array8 = new Array(8).fill(0);
         const kPoly = 0xEDB88320;
-        let i = 0;
-        while (i < 256) {
-            let r = i;
-            let j = 0;
-            while (j < 8) {
-                if ((r & 1) !== 0) {
-                    r = (r >>> 1) ^ kPoly;
-                } else {
-                    r >>>= 1;
-                }
-                j += 1;
-            }
-            table[i] = r;
-            i += 1;
-        }
+        const reducer = (result) => (
+            (result & 1) === 1
+            ? (result >>> 1) ^ kPoly
+            : result >>> 1
+        );
+        const toResult = (ignore, index) => array8.reduce(reducer, index);
+        return array256.map(toResult);
     }());
     const toUint32 = (value) => (
         value < 0
         ? value + 0x100000000
         : value
     );
-    const getDigest = (value) => ~value;
     const updateByte = (value, b) => table[value & 0xFF ^ b] ^ (value >>> 8);
     const update = (data) => data.reduce(updateByte, 0xFFFFFFFF);
     /**
      * @param {Uint8Array|Uint8ClampedArray|number[]} data
      * @returns {number}
      */
-    const calculateDigest = (data) => toUint32(getDigest(update(data)));
+    const calculateDigest = (data) => toUint32(~(update(data)));
     /**
      * @param {number} digest
      * @param {Uint8Array|Uint8ClampedArray|number[]} data
