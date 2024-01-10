c2a14687a5246fe225472ef15019af491548d05c
seabook7 <40333849+seabook7@users.noreply.github.com>
2024-01-10 10:43:32 +0800

    Update crc.js

--- scripts/crc.js
+++ scripts/crc.js
@@ -1,34 +1,79 @@
-/*jslint bitwise*/
-const CRC = (function () {
-    const table = (function () {
-        const array256 = new Uint32Array(256);
-        const array8 = new Uint8Array(8);
-        const kPoly = 0xEDB88320;
-        const reducer = (result) => (
-            (result & 1) === 1
-            ? (result >>> 1) ^ kPoly
-            : result >>> 1
-        );
-        const toResult = (ignore, index) => array8.reduce(reducer, index);
-        return array256.map(toResult);
-    }());
-    const toUint32 = (value) => (
-        value < 0
-        ? value + 0x100000000
-        : value
+// reference:
+// https://www.unicode.org/Public/MAPPINGS/OBSOLETE/EASTASIA/JIS/JIS0201.TXT
+const JISX0201 = (function () {
+    const call = (acc, fn) => fn(acc);
+    const pipe = (...args) => args.reduce(call);
+    const toArray = (codes) => (
+        Array.isArray(codes)
+        ? codes
+        : Array.from(codes)
     );
-    const updateByte = (value, b) => table[value & 0xFF ^ b] ^ (value >>> 8);
-    const update = (data) => data.reduce(updateByte, 0xFFFFFFFF);
+    const isDecodable = (code) => (
+        (code >= 0x20 && code < 0x7F)
+        || (code >= 0xA1 && code < 0xE0)
+    );
+    const filterDecodable = (codes) => codes.filter(isDecodable);
+    const toUnicode = (code) => (
+        code === 0x5C
+        ? 0xA5
+        : code === 0x7E
+        ? 0x203E
+        : code >= 0xA1
+        ? code + 0xFEC0
+        : code
+    );
+    const mapToUnicode = (codes) => codes.map(toUnicode);
+    const fromCharCode = (codes) => String.fromCharCode(...codes);
     /**
-     * @param {Uint8Array|Uint8ClampedArray|number[]} data
-     * @returns {number}
+     * @param {number[]} codes
+     * @returns {string}
      */
-    const calculateDigest = (data) => toUint32(~(update(data)));
+    const decode = (codes) => pipe(
+        codes,
+        toArray,
+        filterDecodable,
+        mapToUnicode,
+        fromCharCode
+    );
+    const toString = (string) => (
+        typeof string === "string"
+        ? string
+        : String(string)
+    );
+    const map = Array.prototype.map;
+    const toCharCode = (char) => char.charCodeAt(0);
+    const mapToCharCode = (string) => map.call(string, toCharCode);
+    const isEncodable = (code) => (
+        (code >= 0x20 && code < 0x5C)
+        || code === 0xA5
+        || (code >= 0x5D && code < 0x7E)
+        || code === 0x203E
+        || (code >= 0xFF61 && code < 0xFFA0)
+    );
+    const filterEncodable = (codes) => codes.filter(isEncodable);
+    const toJISX0201 = (code) => (
+        code === 0xA5
+        ? 0x5C
+        : code === 0x203E
+        ? 0x7E
+        : code >= 0xFF61
+        ? code - 0xFEC0
+        : code
+    );
+    const mapToJISX0201 = (codes) => codes.map(toJISX0201);
     /**
-     * @param {number} digest
-     * @param {Uint8Array|Uint8ClampedArray|number[]} data
-     * @returns {boolean}
+     * @param {string} string
+     * @returns {number[]}
      */
-    const verifyDigest = (digest, data) => calculateDigest(data) === digest;
-    return {calculateDigest, verifyDigest};
+    const encode = (string) => pipe(
+        string,
+        toString,
+        mapToCharCode,
+        filterEncodable,
+        mapToJISX0201
+    );
+    return {
+        decode,
+        encode
+    };
 }());
